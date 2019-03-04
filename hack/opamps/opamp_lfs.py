adf961e8cb54cb636280f0456ffcaed21c00f1f8
Luke Hsiao <lwhsiao@stanford.edu>
2019-03-04 10:41:20 -0800

    broaden opamp matchers and add LFs

--- hack/opamps/opamp_lfs.py
+++ hack/opamps/opamp_lfs.py
@@ -25,6 +25,16 @@ FALSE = 1
 TRUE = 2
 
 
+def high_page_num(c):
+    gain_pg = get_page(c.gain)
+    current_pg = get_page(c.supply_current)
+
+    if gain_pg > 10 or current_pg > 10:
+        return FALSE
+
+    return ABSTAIN
+
+
 # Gain LFs
 def pos_gain(c):
     return TRUE if "gain" in get_row_ngrams(c.gain) else ABSTAIN
@@ -42,8 +52,24 @@ def neg_gain_keywords_in_row(c):
     return (
         FALSE
         if overlap(
-            ["small", "signal", "input", "noise", "f=", "f", "-3", "db", "dbm", "dbc", "voltage", "range"],
-            [_.lower() for _ in get_row_ngrams(c.gain) if _],
+            [
+                "small",
+                "thd",
+                "signal",
+                "flatness",
+                "input",
+                "noise",
+                "f=",
+                "f",
+                "-3",
+                "power",
+                "db",
+                "dbm",
+                "dbc",
+                "voltage",
+                "range",
+            ],
+            get_row_ngrams(c.gain, lower=True),
         )
         else ABSTAIN
     )
@@ -53,8 +79,8 @@ def neg_gain_keywords_in_column(c):
     return (
         FALSE
         if overlap(
-            ["condition", "conditions", "vgn", "f", "-3", "db", "dbc"],
-            [_.lower() for _ in get_col_ngrams(c.gain) if _],
+            ["test", "condition", "conditions", "vgn", "f", "-3", "db", "dbc"],
+            get_col_ngrams(c.gain, lower=True),
         )
         else ABSTAIN
     )
@@ -64,12 +90,34 @@ def neg_gain_keywords_in_column(c):
 def pos_current(c):
     return TRUE if "current" in get_row_ngrams(c.supply_current) else ABSTAIN
 
+
 def neg_current_keywords_in_column(c):
     return (
         FALSE
         if overlap(
-            ["over", "temperature", "vgn", "f", "-3", "db", "dbc"],
-            [_.lower() for _ in get_col_ngrams(c.gain) if _],
+            ["over", "temperature", "vgn", "f", "-3", "db", "dbc", "min", "max"],
+            get_col_ngrams(c.supply_current, lower=True),
+        )
+        else ABSTAIN
+    )
+
+def neg_current_keywords_in_vert(c):
+    return (
+        FALSE
+        if overlap(
+            ["over", "temperature", "vgn", "f", "-3", "db", "dbc", "min", "max"],
+            get_vert_ngrams(c.supply_current, lower=True),
+        )
+        else ABSTAIN
+    )
+
+
+def neg_current_keywords_in_row(c):
+    return (
+        FALSE
+        if overlap(
+            ["output", "drive", "voltage", "io"],
+            get_row_ngrams(c.supply_current, lower=True),
         )
         else ABSTAIN
     )
@@ -81,4 +129,8 @@ opamp_lfs = [
     pos_current,
     neg_gain_keywords_in_row,
     neg_gain_keywords_in_column,
+    neg_current_keywords_in_row,
+    neg_current_keywords_in_vert,
+    neg_current_keywords_in_column,
+    high_page_num,
 ]
