e9ed715c03f89a47fe0aae63a033284608ff2409
Luke Hsiao <lwhsiao@stanford.edu>
2019-03-07 01:09:09 -0800

    checkpoint(opamps): updating labeling functions

--- hack/opamps/opamp_lfs.py
+++ hack/opamps/opamp_lfs.py
@@ -1,12 +1,12 @@
 from fonduer.utils.data_model_utils import (
     get_aligned_ngrams,
-    get_col_ngrams,
     get_cell_ngrams,
+    get_col_ngrams,
     get_head_ngrams,
     get_horz_ngrams,
     get_left_ngrams,
-    get_neighbor_sentence_ngrams,
     get_neighbor_cell_ngrams,
+    get_neighbor_sentence_ngrams,
     get_page,
     get_page_vert_percentile,
     get_right_ngrams,
@@ -42,23 +42,49 @@ def pos_gain(c):
     else:
         ABSTAIN
 
+def pos_gain_keyword(c):
+    col_ngrams = set(get_col_ngrams(c.gain, lower=True))
+    if overlap(["typ.", "typ"], col_ngrams):
+        return TRUE
+    else:
+        ABSTAIN
 
-def pos_gain_keywords(c):
-    return (
-        TRUE
-        if overlap(["typ", "typ."], get_col_ngrams(c.gain, lower=True))
-        else ABSTAIN
+def pos_gain_header_unit(c):
+    row_ngrams = set(get_row_ngrams(c.gain, n_max=1, lower=True))
+    vert_ngrams = set(get_vert_ngrams(c.gain, n_max=1, lower=True))
+    right_ngrams = set(
+        [
+            x[0]
+            for x in get_neighbor_cell_ngrams(
+                c.gain, n_max=1, dist=5, directions=True, lower=False
+            )
+            if x[-1] == "RIGHT"
+        ]
     )
+    if (
+        overlap(["gain", "unity"], row_ngrams)
+        and overlap(["mhz", "khz"], right_ngrams)
+        and overlap(["typ", "typ."], vert_ngrams)
+    ):
+        return TRUE
+    else:
+        return ABSTAIN
+
+
+def neg_gain_bandwidth_missing_gain(c):
+    row_ngrams = set(get_row_ngrams(c.gain, n_max=1, lower=True))
+    if "bandwidth" in row_ngrams and "gain" not in row_ngrams:
+        return FALSE
+
+    return ABSTAIN
 
 
 def neg_gain_keywords_in_cell(c):
-    return (
-        FALSE
-        if overlap(
-            ["g", "vo", "vpp", "f=", "f", "="], get_cell_ngrams(c.gain, lower=True)
-        )
-        else ABSTAIN
-    )
+    cell_ngrams = set(get_cell_ngrams(c.gain, n_max=1, lower=True))
+    if overlap(["g", "vo", "vpp", "f=", "f", "="], cell_ngrams):
+        return FALSE
+    else:
+        return ABSTAIN
 
 
 def neg_gain_too_many_words_in_cell(c):
@@ -66,7 +92,7 @@ def neg_gain_too_many_words_in_cell(c):
     if len(cell_ngrams) >= 4:
         return FALSE
     else:
-        ABSTAIN
+        return ABSTAIN
 
 
 def neg_gain_keywords_in_right_cell(c):
@@ -81,68 +107,106 @@ def neg_gain_keywords_in_right_cell(c):
     )
     if not overlap(["kHz", "MHz", "GHz"], right_ngrams):
         return FALSE
+    else:
+        return ABSTAIN
 
-    return ABSTAIN
 
+def neg_gain_keywords_in_horz(c):
+    horz_ngrams = set(get_horz_ngrams(c.gain, n_max=1, lower=True))
+    if overlap(
+        [
+            "small",
+            "full",
+            "flat",
+            "current",
+            "thd",
+            "signal",
+            "flatness",
+            "input",
+            "noise",
+            "f=",
+            "f",
+            "-3",
+            "power",
+            "db",
+            "dbm",
+            "output",
+            "impedence",
+            "delay",
+            "capacitance",
+            "range",
+            "ratio",
+            "dbc",
+            "temperature",
+            "common",
+            "voltage",
+            "range",
+        ],
+        horz_ngrams,
+    ):
+        return FALSE
+    else:
+        return ABSTAIN
 
 def neg_gain_keywords_in_row(c):
-    return (
-        FALSE
-        if overlap(
-            [
-                "small",
-                "full",
-                "flat",
-                "current",
-                "thd",
-                "signal",
-                "flatness",
-                "input",
-                "noise",
-                "f=",
-                "f",
-                "-3",
-                "power",
-                "db",
-                "dbm",
-                "output",
-                "impedence",
-                "delay",
-                "capacitance",
-                "range",
-                "ratio",
-                "dbc",
-                "temperature",
-                "common",
-                "voltage",
-                "range",
-            ],
-            get_row_ngrams(c.gain, lower=True),
-        )
-        else ABSTAIN
-    )
+    row_ngrams = set(get_row_ngrams(c.gain, n_max=1, lower=True))
+    if overlap(
+        [
+            "small",
+            "full",
+            "flat",
+            "current",
+            "thd",
+            "signal",
+            "flatness",
+            "input",
+            "noise",
+            "f=",
+            "f",
+            "-3",
+            "power",
+            "db",
+            "dbm",
+            "output",
+            "impedence",
+            "delay",
+            "capacitance",
+            "range",
+            "ratio",
+            "dbc",
+            "temperature",
+            "common",
+            "voltage",
+            "range",
+        ],
+        row_ngrams,
+    ):
+        return FALSE
+    else:
+        return ABSTAIN
 
 
 def neg_gain_keywords_in_column(c):
-    return (
-        FALSE
-        if overlap(
-            [
-                "max",
-                "min",
-                "test",
-                "condition",
-                "conditions",
-                "vgn",
-                "f",
-                "-3",
-                "db",
-                "dbc",
-            ],
-            get_col_ngrams(c.gain, lower=True),
-        )
-        else ABSTAIN
-    )
+    col_ngrams = set(get_col_ngrams(c.gain, n_max=1, lower=True))
+    if overlap(
+        [
+            "max",
+            "min",
+            "test",
+            "condition",
+            "conditions",
+            "vgn",
+            "f",
+            "-3",
+            "db",
+            "dbc",
+        ],
+        col_ngrams,
+    ):
+        return FALSE
+
+    else:
+        return ABSTAIN
 
 
 # Supply Current LFs
@@ -200,14 +264,17 @@ def neg_current_keywords_in_row(c):
 
 
 gain_lfs = [
-    pos_gain,
-    pos_gain_keywords,
-    neg_gain_keywords_in_row,
-    neg_gain_keywords_in_column,
+    neg_gain_bandwidth_missing_gain,
     neg_gain_keywords_in_cell,
+    neg_gain_keywords_in_column,
+    neg_gain_keywords_in_horz,
     neg_gain_keywords_in_right_cell,
+    neg_gain_keywords_in_row,
     neg_gain_too_many_words_in_cell,
     neg_low_page_num,
+    pos_gain,
+    pos_gain_header_unit,
+    pos_gain_keyword,
 ]
 
 current_lfs = [
