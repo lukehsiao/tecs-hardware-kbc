70623812afbe464487806e5b340b3d1d16af1acc
Luke Hsiao <lwhsiao@stanford.edu>
2019-10-07 10:54:22 -0700

    feat(plot_fig10): add data and plotting for fig10 analysis
    To support our discussion in Section 4.4:
    
    > In support of this intuition, we find that the heuristic supervision
    > used in the transistor dataset results in higher precision, while the
    > human annotations used in the operational amplifier dataset results in
    > higher recall (Table 2). In light of these observations, we propose
    > that a joint approach leveraging the benefits of both supervision from
    > heuristics and supervision from human annotations successfully
    > incorporates the advantages of each individual approach. For example,
    > when certain candidates are difficult to label heuristically, even a
    > small amount of precise human annotations can provide sufficient
    > signal for a machine-learning model to learn from and apply
    > systematically.
    
    This uses data from transistors ce_v_max relation, and sweeps over 100
    values of b to provide a sense of how the two approaches can affect the
    curves of precision, recall, and f1. The data is from a training set of
    1000 documents.

---
+++ scripts/plot_fig10.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+import argparse
+import logging
+import sys
+from subprocess import DEVNULL, run
+
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from matplotlib.backends.backend_pdf import PdfPages
+
+matplotlib.rcParams["text.usetex"] = True
+
+sns.set(style="whitegrid")
+sns.set_context("paper", font_scale=1.7)
+
+
+# Configure logging for Hack
+logging.basicConfig(
+    stream=sys.stdout,
+    format="[%(asctime)s][%(levelname)s] %(name)s:%(lineno)s - %(message)s",
+    level=logging.INFO,
+)
+logger = logging.getLogger(__name__)
+
+FIGSIZE = (7, 3)
+
+
+def _plot(infile):
+    """Plotting logic."""
+    fig, ax = plt.subplots(figsize=FIGSIZE)
+
+    # type,b,f1,precision,recall
+    fig10_data = pd.read_csv(infile, skipinitialspace=True)
+
+    # Plot precision
+    plot = sns.lineplot(
+        data=fig10_data, hue="type", style="type", y="precision", x="b", ax=ax
+    )
+
+    # Remove the seaborn legend title
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles=handles[1:], labels=labels[1:])
+    ax.set_ylim([0, 1])
+    ax.set_xlim([0, 1])
+
+    sns.despine(bottom=True, left=True)
+    plot.set(xlabel="Threshold")
+    plot.set(ylabel="Precision")
+    outfile = "fig10_precision.pdf"
+    pp = PdfPages(outfile)
+    pp.savefig(plot.get_figure().tight_layout())
+    pp.close()
+    run(["pdfcrop", outfile, outfile], stdout=DEVNULL, check=True)
+    logger.info(f"Plot saved to {outfile}")
+
+    # Plot recall
+    fig, ax = plt.subplots(figsize=FIGSIZE)
+    plot = sns.lineplot(
+        data=fig10_data, hue="type", style="type", y="recall", x="b", ax=ax
+    )
+
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles=handles[1:], labels=labels[1:])
+    ax.set_ylim([0, 1])
+    ax.set_xlim([0, 1])
+
+    sns.despine(bottom=True, left=True)
+    plot.set(xlabel="Threshold")
+    plot.set(ylabel="Recall")
+    outfile = "fig10_recall.pdf"
+    pp = PdfPages(outfile)
+    pp.savefig(plot.get_figure().tight_layout())
+    pp.close()
+    run(["pdfcrop", outfile, outfile], stdout=DEVNULL, check=True)
+    logger.info(f"Plot saved to {outfile}")
+
+    # Plot f1
+    fig, ax = plt.subplots(figsize=FIGSIZE)
+    plot = sns.lineplot(data=fig10_data, hue="type", style="type", y="f1", x="b", ax=ax)
+
+    # Remove the seaborn legend title
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles=handles[1:], labels=labels[1:])
+    ax.set_ylim([0, 1])
+    ax.set_xlim([0, 1])
+
+    sns.despine(bottom=True, left=True)
+    plot.set(xlabel="Threshold")
+    plot.set(ylabel="F1 Score")
+    outfile = "fig10_f1.pdf"
+    pp = PdfPages(outfile)
+    pp.savefig(plot.get_figure().tight_layout())
+    pp.close()
+    run(["pdfcrop", outfile, outfile], stdout=DEVNULL, check=True)
+    logger.info(f"Plot saved to {outfile}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data", type=str, default="fig10.csv", help="CSV file of fig10 data"
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="verbose",
+        action="store_true",
+        help="Output INFO level logging.",
+    )
+    args = parser.parse_args()
+
+    if args.verbose:
+        ch = logging.StreamHandler()
+        logger.setLevel(logging.INFO)
+        ch.setLevel(logging.INFO)
+        formatter = logging.Formatter("[%(levelname)s] %(name)s - %(message)s")
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+
+    _plot(args.data)
