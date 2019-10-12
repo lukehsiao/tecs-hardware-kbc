9e539bedca69a6159519420584a4ea42710132bf
Luke Hsiao <lwhsiao@stanford.edu>
2019-10-12 14:59:56 -0700

    chore(scripts): drop --gpu from scale_rels scripts
    For multi-task learning, we now longer have the GPU flag. This drops it
    from the script so we can re-run the scaling experiments using emmental
    multi-task learning.

--- scripts/scaling_rels.sh
+++ scripts/scaling_rels.sh
@@ -5,7 +5,7 @@ dropdb --if-exists transistor_scale_rels
 createdb transistor_scale_rels
 
 # Only parse the first time, no need to reparse the same docs over and over.
-transistors --log-dir transistor_scale_rels_logs --gpu 0 --conn-string "postgresql:///transistor_scale_rels" --parallel=8 --max-docs=1000 --parse --first-time --stg-temp-min 
+transistors --log-dir transistor_scale_rels_logs --conn-string "postgresql:///transistor_scale_rels" --parallel=8 --max-docs=1000 --parse --first-time --stg-temp-min 
 # Truncate, which is faster and more similar to a fresh DB than dropping via query.
 psql -d transistor_scale_rels -c "truncate mention cascade;"
 psql -d transistor_scale_rels -c "truncate candidate cascade;"
@@ -14,18 +14,18 @@ psql -d transistor_scale_rels -c "truncate feature_key cascade;"
 psql -d transistor_scale_rels -c "vacuum analyze;"
 
 
-transistors --log-dir transistor_scale_rels_logs --gpu 0 --conn-string "postgresql:///transistor_scale_rels" --parallel=8 --max-docs=1000 --first-time --stg-temp-min --stg-temp-max
+transistors --log-dir transistor_scale_rels_logs --conn-string "postgresql:///transistor_scale_rels" --parallel=8 --max-docs=1000 --first-time --stg-temp-min --stg-temp-max
 psql -d transistor_scale_rels -c "truncate mention cascade;"
 psql -d transistor_scale_rels -c "truncate candidate cascade;"
 psql -d transistor_scale_rels -c "truncate label_key cascade;"
 psql -d transistor_scale_rels -c "truncate feature_key cascade;"
 psql -d transistor_scale_rels -c "vacuum analyze;"
 
-transistors --log-dir transistor_scale_rels_logs --gpu 0 --conn-string "postgresql:///transistor_scale_rels" --parallel=8 --max-docs=1000 --first-time --stg-temp-min --stg-temp-max --polarity
+transistors --log-dir transistor_scale_rels_logs --conn-string "postgresql:///transistor_scale_rels" --parallel=8 --max-docs=1000 --first-time --stg-temp-min --stg-temp-max --polarity
 psql -d transistor_scale_rels -c "truncate mention cascade;"
 psql -d transistor_scale_rels -c "truncate candidate cascade;"
 psql -d transistor_scale_rels -c "truncate label_key cascade;"
 psql -d transistor_scale_rels -c "truncate feature_key cascade;"
 psql -d transistor_scale_rels -c "vacuum analyze;"
 
-transistors --log-dir transistor_scale_rels_logs --gpu 0 --conn-string "postgresql:///transistor_scale_rels" --parallel=8 --max-docs=1000 --first-time --stg-temp-min --stg-temp-max --polarity --ce-v-max
+transistors --log-dir transistor_scale_rels_logs --conn-string "postgresql:///transistor_scale_rels" --parallel=8 --max-docs=1000 --first-time --stg-temp-min --stg-temp-max --polarity --ce-v-max
