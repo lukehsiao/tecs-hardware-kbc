df5ecfa122c160b1baab3b5f4a5e244f30216b5b
Luke Hsiao <lwhsiao@stanford.edu>
2020-02-28 17:10:57 -0800

    build: add mouser dataset to download script

--- hack/opamps/download_data.sh
+++ hack/opamps/download_data.sh
@@ -15,3 +15,21 @@ echo "Deleting $data_tar..."
 rm $data_tar
 
 echo "Done!"
+
+echo "Downloading mouser op-amp dataset..."
+url=https://stanford.box.com/shared/static/568p14l4zxnfbgtrtzpm8wr2ynp2328l.xz
+data_tar=mouser_opamps.tar.xz
+
+if type curl &>/dev/null; then
+    curl -RL --retry 3 -C - $url -o $data_tar
+elif type wget &>/dev/null; then
+    wget -N $url -O $data_tar
+fi
+
+echo "Unpacking mouser op-amp dataset..."
+tar vxf $data_tar -C data
+
+echo "Deleting $data_tar..."
+rm $data_tar
+
+echo "Done!"
