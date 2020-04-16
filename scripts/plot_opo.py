9dc79d0e099fdc074951bdc4b3d3bfd5ca6e2e55
Luke Hsiao <lwhsiao@stanford.edu>
2020-04-15 18:47:23 -0700

    refactor(scripts/plot_opo): use defaults for latest data

--- scripts/plot_opo.py
+++ scripts/plot_opo.py
@@ -257,7 +257,7 @@ if __name__ == "__main__":
         default="../hack/opamps/output_current.csv",
         help="CSV file name to plot",
     )
-    parser.add_argument("-gb", type=float, help="Gain threshold to use.", default=0.40)
+    parser.add_argument("-gb", type=float, help="Gain threshold to use.", default=0.50)
     parser.add_argument(
         "-cb", type=float, help="Current threshold to use.", default=0.75
     )
